8f3bd60cd75694fe12f598fa805a21ca682291a8
Dylan Zemlin <dylansmrw@gmail.com>
2022-02-12 15:22:25 -0600

    Added deltaTime and basic map drawing

--- src/scripts/stages/defense.js
+++ src/scripts/stages/defense.js
@@ -1,20 +1,49 @@
 const FPS = 60;
 
+class GameMap {
+    constructor(data) {
+        this.data = data;
+    }
+
+    draw(canvas, context) {
+        // Draw the ground first
+        const { width, height } = canvas;
+        context.fillStyle = data.groundColor;
+        context.fillRect(0, 0, width, height);
+    }
+}
+
 class GameStageDefense {
     constructor() {
         this.log("Initialized");
 
-        const canvas = document.getElementById("towerCanvas");
-        this.context = canvas.getContext("2d");
+        this.canvas = document.getElementById("towerCanvas");
+        this.context = this.canvas.getContext("2d");
         this.timerId = undefined;
+        this.lastFrame = 0;
+        this.map = new GameMap({
+            map_name: "Default Map",
+            groundColor: "#567d46",
+            path: [
+                {  }
+            ]
+        });
     }
 
-    onUpdate() {
+    onUpdate(deltaTime) {
+        this.lastFrame = Date.now();
 
+        // Clear Canvas
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // First draw the map
+        this.map.draw(this.canvas, this.context);
     }
 
     onSceneLoad() {
-        this.timer = setInterval(() => this.onUpdate(), 1000 / FPS);
+        this.timer = setInterval(() => {
+            this.onUpdate((Date.now() - this.lastFrame) / 1000.0)
+        }, 1000 / FPS);
     }
 
     onSceneUnload() {
